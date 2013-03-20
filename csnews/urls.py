ace7e4794fc16159e0fbcafd7f567c443b28c8a2
Urtzi Odriozola <uodriozola@codesyntax.com>
2013-03-20 15:48:27 +0100

    Update urls.py
    url reference bug fixed

--- csnews/urls.py
+++ csnews/urls.py
@@ -4,9 +4,9 @@ from csnews.feeds import LatestNews
 #feed_dict = {'rss': LatestNews}
 
 urlpatterns = patterns('',
-    (r'^$','ahotsak.news.views.index'),
+    (r'^$','csnews.views.index'),
     (r'^feed-(?P<url>.*)/$', LatestNews()),
-    (r'^hemeroteka/', 'ahotsak.news.views.hemeroteka'),            
-    (r'^(?P<article_slug>[\-\d\w]+)/$','ahotsak.news.views.article_index'),
+    (r'^hemeroteka/', 'csnews.views.hemeroteka'),            
+    (r'^(?P<article_slug>[\-\d\w]+)/$','csnews.views.article_index'),
 )
 
