60b863d86528ccf4909cddaf45dcd7a437c2754d
Urtzi Odriozola <uodriozola@codesyntax.com>
2013-03-20 15:47:28 +0100

    Update urls.py
    eguneko bideoa is not part of this project

--- csnews/urls.py
+++ csnews/urls.py
@@ -6,9 +6,7 @@ from csnews.feeds import LatestNews
 urlpatterns = patterns('',
     (r'^$','ahotsak.news.views.index'),
     (r'^feed-(?P<url>.*)/$', LatestNews()),
-    (r'^hemeroteka/', 'ahotsak.news.views.hemeroteka'),  
-    url(r'^eguneko-bideoa/$', 'ahotsak.news.views.eguneko_pasartea_index',name='eguneko_pasartea_index'),
-    url(r'^eguneko-bideoa/(?P<data>[\-\d\w]+)/$', 'ahotsak.news.views.eguneko_pasartea',name='eguneko_pasartea'),          
+    (r'^hemeroteka/', 'ahotsak.news.views.hemeroteka'),            
     (r'^(?P<article_slug>[\-\d\w]+)/$','ahotsak.news.views.article_index'),
 )
 
