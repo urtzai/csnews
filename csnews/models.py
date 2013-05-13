f65de7aefddca7df3c4b38f8ccf951c3dd4d0c46
urtzai <uodriozola@codesyntax.com>
2013-05-13 17:33:30 +0200

    model changed

--- csnews/models.py
+++ csnews/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from tagging.fields import TagField
 from django.utils.translation import ugettext_lazy as _
 from photologue.models import Photo
 from django.db.models.signals import post_save
@@ -22,14 +21,10 @@ class Article(models.Model):
     body = models.TextField(_('body'))
     image = models.ForeignKey(Photo,null=True,blank=True,related_name=_('news image'))
         
-    tags = TagField()
-
     is_public = models.BooleanField(default=True)
-    allow_comments = models.BooleanField(_('allow comments'), default=True)
 
     added = models.DateField(_('added'),auto_now_add=True)
     modified = models.DateField(_('modified'),auto_now=True)
-    sent_by_email =  models.BooleanField(default=False,editable=False)
 
     def get_title(self):
         return self.title
@@ -43,33 +38,4 @@ class Article(models.Model):
 
     def __unicode__(self):
         return u'%s' % self.title
-
-    def get_absolute_url(self):
-        return "/albisteak/%s" % (self.slug)
     
-def send_by_email(sender, instance, created,**kwargs):
-    now = datetime.datetime.now()
-    send = False
-    if created:
-        if instance.is_public and instance.published < now:
-            send = True
-    else:
-        if instance.is_public and instance.published < now and not(instance.sent_by_email):
-            send = True
-
-    if send:
-        subject = instance.title
-        message = """
-        %s
-
-        %s
-        """ % (instance.summary, instance.body)
-        msgf = getattr(settings,'BULETIN_EMAIL',None)
-        msgt = getattr(settings,'DEFAULT_POSTA_ZERRENDA',None)
-        html_msg = render_to_string('news/article_by_mail.html',{'article':instance,'STATIC_URL':getattr(settings,'STATIC_URL'),'BASE_PATH':'www.ahotsak.com'})
-        msg = EmailMultiAlternatives(subject, message, msgf, [msgt])
-        msg.attach_alternative(html_msg, "text/html")
-        msg.send()
-        instance.sent_by_email=True
-        instance.save()
-post_save.connect(send_by_email, sender=Article)
