830c1ed1e889be472ed81fe76fe9f381aa252f9d
Urtzi Odriozola <uodriozola@codesyntax.com>
2017-03-17 10:13:41 +0100

    code cleaned

--- csnews/admin.py
+++ csnews/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from csnews.models import Article
-from django.conf import settings
 
 from tinymce.widgets import TinyMCE
 
@@ -16,38 +15,12 @@ show_entry_thumbnail.allow_tags = True
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title_es', 'published', 'is_public', show_entry_thumbnail)
-    list_display_links = ('id', 'title_es')
+    list_display = ('id', 'title', 'published', 'is_public', show_entry_thumbnail)
+    list_display_links = ('id', 'title')
     ordering = ('-id',)
-    search_fields = ['title_es', 'summary_es']
-    prepopulated_fields = {'slug_es': ('title_es',), 'slug_eu': ('title_eu',)}
+    search_fields = ['title', 'summary']
+    prepopulated_fields = {'slug': ('title',)}
     photologue_image_fields = ('image',)
 
-    class Media:
-        js = (getattr(settings, 'STATIC_URL', '') + 'tiny_mce/tiny_mce.js',
-              )
 
-
-class TinyMCEArticleAdmin(ArticleAdmin):
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name in ('body_es', 'body_eu'):
-            return db_field.formfield(widget=TinyMCE(
-                attrs={'cols': 80, 'rows': 30},
-                mce_attrs={
-                    'theme': "advanced",
-                    'mode': "textareas",
-                    'plugins': "media",
-                    'extended_valid_elements': "iframe[src|width|height|name|align]",
-                    'theme_advanced_buttons1': "formatselect,bold,italic,underline,separator,justifyleft,justifycenter,justifyright, justifyfull,bullist,numlist,undo,redo,link,unlink,image,code,removeformat",
-                    'theme_advanced_buttons2': "",
-                    'theme_advanced_buttons3': "",
-                    'theme_advanced_toolbar_location': "top",
-                    'theme_advanced_toolbar_align': "left",
-                    'theme_advanced_resizing': 'false',
-                    'media_strict': 'false',
-                },
-            ))
-        return super(TinyMCEArticleAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-
-
-admin.site.register(Article, TinyMCEArticleAdmin)
+admin.site.register(Article, ArticleAdmin)
--- csnews/views.py
+++ csnews/views.py
@@ -3,45 +3,19 @@ from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 
 from csnews.models import Article
-from csnews.diggpaginator import DiggPaginator
-
-import time
-from datetime import datetime
 
 ARTICLE_NUMBER_PER_PAGE = 20
 
-def _get_page(list,page):
-    """ """
-    paginator = DiggPaginator(list, ARTICLE_NUMBER_PER_PAGE, body=5, padding=2)
-    try:
-        page = int(page)
-    except ValueError:
-        page = 1
-
-    try:
-        tor = paginator.page(page)
-    except:
-        tor = paginator.page(paginator.num_pages)
-    return tor
 
 def index(request):
-    """ """
-    h = {}
-    
-    h['articles'] = Article.objects.filter(is_public=True)
-    h['page'] = _get_page(h['articles'],request.GET.get('orria', '1'))    
-    return render_to_response('news/articles.html',h,context_instance=RequestContext(request))
+    articles = Article.objects.filter(is_public=True)
+    return render_to_response('news/articles.html', locals(), context_instance=RequestContext(request))
 
-def article_index(request,article_slug):
-    """ """
-    h = {}
-    if request.LANGUAGE_CODE == 'eu':
-        h['obj'] = get_object_or_404(Article, slug_eu = article_slug)
-    else:
-        h['obj'] = get_object_or_404(Article, slug_es = article_slug)
-    return render_to_response('news/article.html',h,context_instance=RequestContext(request))
+
+def article_index(request, article_slug):
+    obj = get_object_or_404(Article, slug=article_slug)
+    return render_to_response('news/article.html', locals(), context_instance=RequestContext(request))
+
 
 def hemeroteka(request):
-    """ """
-    h = {}
-    return render_to_response('news/hemeroteka.html',h,context_instance=RequestContext(request))
+    return render_to_response('news/hemeroteka.html', locals(), context_instance=RequestContext(request))
